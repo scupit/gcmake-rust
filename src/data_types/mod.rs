ecbe255408c735bc9f6bf97c4f71c548689f4823
Skylar Cupit <skylarcupit@gmail.com>
2021-04-30 22:27:12 -0500

    Some more basic syntax

--- src/data_types/mod.rs
+++ src/data_types/mod.rs
@@ -1,14 +1,31 @@
+use std::collections::{HashMap, HashSet};
+
 use serde::{Serialize, Deserialize};
 
 #[derive(Serialize, Deserialize, Debug)]
-pub struct Project {
+pub struct RawProject {
   name: String,
   version: String,
-  languages: Vec<String>,
-  supported_compilers: Vec<String>
+  languages: HashSet<String>,
+  supported_compilers: HashSet<CompilerSpecifier>,
+  output: HashMap<String, RawCompiledItem>
 }
 
-// #[derive(Serialize, Deserialize)]
-// pub struct OutputData {
-//   outputType: String
-// }
+#[derive(Serialize, Deserialize, Debug)]
+pub enum CompiledItemType {
+  Executable,
+  StaticLib,
+  SharedLib
+}
+
+#[derive(Serialize, Deserialize, Hash, Debug, Eq, PartialEq, PartialOrd, Ord)]
+pub enum CompilerSpecifier {
+  GCC,
+  MSVC,
+  Clang
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct RawCompiledItem {
+  output_type: CompiledItemType
+}
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 mod data_types;
 
 use std::fs;
-use data_types::Project;
+use data_types::*;
 
 
 fn main() {
     
   let project = fs::read_to_string("./gcmake-rust-test-project/cmake_data.yaml").unwrap();
-  let serialized_project: Project = serde_yaml::from_str(&project).unwrap();
+  let serialized_project: RawProject = serde_yaml::from_str(&project).unwrap();
   println!("Project in YAML: {:?}", serialized_project);
 }
